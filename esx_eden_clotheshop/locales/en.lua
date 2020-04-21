52f2b1daca7199e6a0de6ff02b70cb98b665ffd0
AfghanCamper <64086955+AfghanCamper@users.noreply.github.com>
2020-04-21 10:08:04 -0400

    Update en.lua
    english

--- esx_eden_clotheshop/locales/en.lua
+++ esx_eden_clotheshop/locales/en.lua
@@ -1,19 +1,20 @@
-Locales['en'] = {
-	['valid_this_purchase'] = 'Bạn có muốn mua không?',
-  	['yes_valid_purchase'] = 'Mua <span style="color: green;">$%s</span>',
-	['yes'] = 'Có',
-	['no'] = 'Không',
-	['name_outfit'] = 'Tên Trang Phục?',
-	['not_enough_money'] = 'Bạn không đủ tiền',
-	['press_menu'] = 'Bam ~INPUT_CONTEXT~ de mo menu',
-	['clothes'] = 'CUA HANG QUAN AO',
-	['you_paid'] = 'Ban mua ~g~$',
-	['save_in_dressing'] = 'Bạn có muốn lưu lại trang phục không?',
-	['shop_clothes'] = 'Cửa hàng quần áo',
-	['player_clothes'] = 'Phòng thay đồ',
-	['shop_main_menu'] = 'Xin chào',
-	['saved_outfit'] = 'Lưu lại đồ đã mua',
-	['loaded_outfit'] = 'Bạn đã chọn lại đồ đã mua',
-	['suppr_cloth'] = 'Xóa quần áo đã lưu ',
-	['supprimed_cloth'] = 'Quần áo lưa đã được xóa'
+  Locales['en'] = {
+
+	['valid_this_purchase'] = 'validate this purchase?',
+	['yes'] = 'yes',
+	['no'] = 'no',
+	['name_outfit'] = 'name of the outfit?',
+	['not_enough_money'] = 'you do not have enough money',
+	['press_menu'] = 'press ~INPUT_CONTEXT~ to access the menu',
+	['clothes'] = 'clothes',
+	['you_paid'] = 'you paid $',
+	['save_in_dressing'] = 'Do you want to give a name to your outfit ?',
+	['shop_clothes'] = 'clothing store',
+	['player_clothes'] = 'change clothes - dressing-room',
+	['shop_main_menu'] = 'welcome ! what do you want to do ?',
+	['saved_outfit'] = 'your outfit has been saved in your dressing-room. Thank you for your visit !',
+	['loaded_outfit'] = 'you have recovered the outfit of your dressing-room. Thank you for your visit !',
+	['suppr_cloth'] = 'delete outfit - dressing-room',
+	['supprimed_cloth'] = 'this outfit has been deleted of your dressing-room'
+
 }
